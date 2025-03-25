9ff24baf4eb77bc9b425296c84b26e8ff53d626c
B Venkatesh <venkumj1234@gmail.com>
2025-03-26 00:35:50 +0530

    env variables added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useSendTransaction } from "wagmi";
-import { parseEther } from "viem";
 import { useAccount } from "wagmi";
+import { ethers as krnlEthers } from "krnl-sdk";
+import { ethers } from "ethers";
+import { contractABI } from "../utils/contractABI";
 
 export default function Home() {
   const { address } = useAccount();
-  const { data: hash, sendTransaction } = useSendTransaction();
   const [counter, setCounter] = useState(0);
+  const provider = new krnlEthers.JsonRpcProvider(
+    "https://v0-0-3-rpc.node.lat"
+  );
+  const provider2 = new ethers.JsonRpcProvider(
+    "https://ethereum-sepolia-rpc.publicnode.com"
+  );
+  const abiCoder = new ethers.AbiCoder();
+  const textInput = "Message Changed";
+  const contractAddress = "0xa13978eC1c11400145fd42C88f6Fb1aC6b6Aff35";
+  const contract = new ethers.Contract(contractAddress, contractABI, provider2);
 
   const fetchCounter = async () => {
     const response = await axios.get("/api/counter");
@@ -20,43 +30,67 @@ export default function Home() {
     fetchCounter();
   };
 
-  // async function executeKrnl() {
-  //   const krnlPayload = await provider.executeKernels(
-  //     entryId,
-  //     accessToken,
-  //     kernelRequestData,
-  //     functionParams
-  //   );
-  //   return krnlPayload;
-  // }
+  const entryId = process.env.NEXT_PUBLIC_ENTRY_ID;
+  const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
-  // async function callContractProtectedFunction(executeResult) {
-  //   const krnlPayload = {
-  //     auth: executeResult.auth,
-  //     kernelResponses: executeResult.kernel_responses,
-  //     kernelParams: executeResult.kernel_params,
-  //   };
-  //   const tx = await contract.protectedFunction(krnlPayload, textInput);
-  //   return tx.hash;
-  // }
+  const kernelRequestData = {
+    senderAddress: address as string,
+    kernelPayload: {
+      "1269": {
+        // off-chain with openapi schema GET method
+        parameters: {
+          header: {},
+          body: {},
+          query: {},
+          path: {},
+        },
+      },
+    },
+  };
+
+  const functionParams = abiCoder.encode(["string"], [textInput]);
+
+  console.log("Function Params", functionParams);
+
+  async function executeKrnl() {
+    const krnlPayload = await provider.executeKernels(
+      entryId as string,
+      accessToken as string,
+      kernelRequestData,
+      functionParams
+    );
+    return krnlPayload;
+  }
+
+  //@ts-expect-error it can be wrong
+  async function callContractProtectedFunction(executeResult) {
+    const krnlPayload = {
+      auth: executeResult.auth,
+      kernelResponses: executeResult.kernel_responses,
+      kernelParams: executeResult.kernel_params,
+    };
+    const tx = await contract.protectedFunction(krnlPayload, textInput);
+    return tx.hash;
+  }
 
   const sendTxn = async () => {
-    // try {
-    //   const krnlPayload = await executeKrnl();
-    //   if (krnlPayload) {
-    //     console.log(krnlPayload);
-    //     const tx = await callContractProtectedFunction(krnlPayload);
-    //     console.log("Transaction Hash is:", tx);
-    //   }
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    console.log("Sending transaction");
-    sendTransaction({
-      to: "0x97A907011c57AD4880aeE2245705234328330c6c",
-      value: parseEther("0.0001"),
-    });
+    console.log("Sending Transaction");
+    try {
+      const krnlPayload = await executeKrnl();
+      if (krnlPayload) {
+        console.log("Got the Kernel Payload", krnlPayload);
+        const tx = await callContractProtectedFunction(krnlPayload);
+        console.log("Transaction Hash is:", tx);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  // const readMessage = async () => {
+  //   const message = await contract.readMessage();
+  //   console.log("Message is:", message);
+  // };
 
   useEffect(() => {
     fetchCounter();
@@ -79,18 +113,16 @@ export default function Home() {
       {address && (
         <div className="flex flex-col items-center mt-4">
           <span className="text-lg text-gray-500 mt-2">
-            You can only send transaction if counter is greater than 5.
+            When you click on send transaction, it will send 0.000{counter} ETH
           </span>
           <button
             onClick={sendTxn}
             className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
           >
-            Send 0.0001 ETH
+            Send ETH
           </button>
         </div>
       )}
-
-      {hash && <div>Transaction Hash: {hash}</div>}
     </div>
   );
 }
